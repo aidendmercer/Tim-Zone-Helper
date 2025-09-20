edcf73da4a063714b396046a35c2518a0c5a9425
aidendmercer <richgenlife@gmail.com>
2025-09-20 15:01:22 +0300

    Delete types/luxon-fix.d.ts

--- types/luxon-fix.d.ts
+++
@@ -1,5 +0,0 @@
-// Ensure TypeScript always finds Luxon's bundled type definitions.
-// This DOES NOT add any third-party types; it points to Luxon's own .d.ts.
-declare module "luxon" {
-  export * from "luxon/build/node/luxon";
-}
