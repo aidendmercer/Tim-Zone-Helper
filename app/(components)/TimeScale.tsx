4c5957bc8c4801f8a666fd2d4076f491830e1243
aidendmercer <richgenlife@gmail.com>
2025-09-20 16:21:33 +0300

    Update TimeScale.tsx

--- app/(components)/TimeScale.tsx
+++ app/(components)/TimeScale.tsx
@@ -3,20 +3,31 @@
 import { hourRangeArray, formatHourLabel } from "../(lib)/time";
 import type { TimeFormat } from "../(lib)/time";
 
-export default function TimeScale({ pxPerHour, timeFormat }: { pxPerHour: number; timeFormat: TimeFormat }) {
+export default function TimeScale({
+  pxPerHour,
+  timeFormat,
+  labelStep = 1
+}: {
+  pxPerHour: number;
+  timeFormat: TimeFormat;
+  /** show a label every N hours (mobile: 2) */
+  labelStep?: 1 | 2 | 3 | 4;
+}) {
   return (
     <div className="relative w-full">
       <div
-        className="timeline-grid h-8 rounded-lg border border-white/10 bg-dayperiod-day/20"
+        className="timeline-grid h-8 rounded-lg border border-slate-200 bg-white"
         style={{ backgroundSize: `${pxPerHour}px 100%` }}
         aria-hidden
       />
       <div className="pointer-events-none absolute inset-0 flex">
         {hourRangeArray().slice(0, 24).map((h) => (
           <div key={h} className="flex-1 relative">
-            <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 hour-labels">
-              {formatHourLabel(h, timeFormat)}
-            </div>
+            {h % labelStep === 0 && (
+              <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 hour-labels">
+                {formatHourLabel(h, timeFormat)}
+              </div>
+            )}
           </div>
         ))}
       </div>
