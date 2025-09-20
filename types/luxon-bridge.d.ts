464a8521c1420077dce109e023dc6e46fbfa74a4
aidendmercer <richgenlife@gmail.com>
2025-09-20 15:20:51 +0300

    Delete types/luxon-bridge.d.ts

--- types/luxon-bridge.d.ts
+++
@@ -1,5 +0,0 @@
-// Bridge TypeScript to Luxon's bundled declaration file.
-// No third-party typings; this re-exports Luxon's own types.
-declare module "luxon" {
-  export * from "luxon/types/luxon";
-}
